ec60de8793c3314dcaf97486885e0ed390f7d681
Max Koning <maxkoning@Maxs-iMac.local>
2018-03-24 16:16:24 +0100

    adjusted login and register route to work better.

--- api/user.routes.v1.js
+++ api/user.routes.v1.js
@@ -14,40 +14,6 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-routes.post('/register', function (req, res) {
-    const userPassword = req.body.password;
-    const userEmail = req.body.email;
-    const emailToken = Math.floor(Math.random() * 10000);
-
-
-    User.findOne({email: userEmail}).then((user) => {
-      if (!user) {
-
-    var mailOptions = {
-      from: 'noreply@spaherkansing.nl',
-      to: userEmail,
-      subject: 'Confirm your Email address',
-      html: '<h2>Thank you for registering</h2> <h4>This is the token to confirm your Email address:</h4>' + emailToken
-    };
-
-        User.create({email: userEmail, password: userPassword, emailVerifyToken: emailToken})
-            .then((user) => {
-              transporter.sendMail(mailOptions, function(error, info){
-              if (error) {
-                console.log(error);
-              } else {
-                console.log('Email sent: ' + info.response);
-              }
-            });
-                res.status(200).send(user);
-            })
-      } else {
-        res.status(401).json({succes: false, message: 'User already exists'})
-      }
-    })
-        .catch((error) => res.status(400).json(error));
-});
-
 routes.post('/verify', function (req, res) {
   const userEmail = req.body.email;
   const userVerifyToken = req.body.emailVerifyToken;
@@ -107,6 +73,40 @@ routes.post('/login', function (req, res) {
     }
   })
   .catch((error) => res.status(401).json({success: false, message: 'login failed. User not found.'}))
+});
+
+routes.post('/register', function (req, res) {
+    const userPassword = req.body.password;
+    const userEmail = req.body.email;
+    const emailToken = Math.floor(Math.random() * 10000);
+
+
+    User.findOne({email: userEmail}).then((user) => {
+      if (!user) {
+
+    var mailOptions = {
+      from: 'noreply@spaherkansing.nl',
+      to: userEmail,
+      subject: 'Confirm your Email address',
+      html: '<h2>Thank you for registering</h2> <h4>This is the token to confirm your Email address:</h4>' + emailToken
+    };
+
+        User.create({email: userEmail, password: userPassword, emailVerifyToken: emailToken})
+            .then((user) => {
+              transporter.sendMail(mailOptions, function(error, info){
+              if (error) {
+                console.log(error);
+              } else {
+                console.log('Email sent: ' + info.response);
+              }
+            });
+                res.status(200).send(user);
+            })
+      } else {
+        res.status(401).json({succes: false, message: 'User already exists'})
+      }
+    })
+        .catch((error) => res.status(400).json(error));
 });
 
 module.exports = routes;
